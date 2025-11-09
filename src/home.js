d5a9605fffb106e393293162a3950503ce1d2b84
ThiagoFigueiredoFilho <thiagofigueiredofilho@gmail.com>
2025-11-09 03:26:26 -0300

    ajustei o login na minha opinião ta ok

--- src/home.js
+++ src/home.js
@@ -2,3 +2,13 @@ const imageUser =  document.querySelector("#user")
 const srcImage = "./src/assets/User_Icon.png"
 
 imageUser.src =  imageUser.src ||  srcImage
+
+const logoutBtn = document.getElementById('logoutbtn');
+
+if (logoutBtn) {
+  logoutBtn.addEventListener('click', (e) => {
+    e.preventDefault(); // impede o link de mudar antes da limpeza
+    sessionStorage.clear(); // apaga todos os dados salvos
+    window.location.href = 'login.html'; // redireciona pro login
+  });
+}
